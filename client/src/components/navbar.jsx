8554bb9aa8d7964b9c9a2ade9d27895d42679a6e
Eh1z <godsentehiz11@gmail.com>
2025-01-09 11:47:06 +0100

    CTA section done

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -4,7 +4,7 @@ import { assets } from "../assets/";
 const Navbar = () => {
 	return (
 		<nav className="w-full py-3 ~px-5/32 flex justify-between items-center ">
-			<img src={assets.logo} alt="logo" />
+			<img src={assets.logo} alt="logo" className="~w-32/52" />
 			<button className="primaryBtn">Get Started</button>
 		</nav>
 	);
