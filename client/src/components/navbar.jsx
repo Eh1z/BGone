e6ed671dd2eaab2898c58434b6f6237664c6f907
Eh1z <godsentehiz11@gmail.com>
2025-01-25 11:08:43 +0100

    added clerk authentication

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import { assets } from "../assets/";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 const Navbar = () => {
+	const { openSignIn } = useClerk();
+	const { isSignedIn, user } = useUser();
 	return (
 		<nav className="w-full ~py-3/8 ~px-3/32 flex justify-between items-center ">
-			<img src={assets.logo} alt="logo" className="~w-32/52" />
-			<button className="primaryBtn">Get Started</button>
+			<a href="/">
+				<img src={assets.logo} alt="logo" className="~w-32/52" />
+			</a>
+			{isSignedIn ? (
+				<div>
+					<UserButton />
+				</div>
+			) : (
+				<button onClick={() => openSignIn({})} className="primaryBtn">
+					Get Started
+				</button>
+			)}
 		</nav>
 	);
 };
